8fbacd3a2930a855e94f96ddc841331d1d5ea882
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-10 21:32:58 +0500

    chapter 2 - 5 complete...

---
+++ JS/class 02/app.js
@@ -0,0 +1,72 @@
+// alert("Hello World");
+// document.write("Hello World");
+// console.log("Hello World");
+
+// var a = 5;
+// var b = 10;
+// var ali = "hamza123";
+// var _murtaza = 567;
+
+// var a = 10;
+// var b = 15;
+// var c = a + b;
+// alert(c);
+
+// var x = "ali";
+// var y = "mughal";
+// var z = x + y;
+// alert(z);
+
+// var f = "faraz";
+// var g = 15;
+// var h = f-g;
+// alert(h)
+
+// var a = "15";
+// var b = 25;
+// var c = a+b;
+// alert(c)
+
+// var x = 3;
+// var y = "6";
+// var z = x + y;
+// alert(z);
+
+// var a;
+// a = 6;
+
+// var a = (3 + 6) / 2 * (5 - 7);
+// alert(a);
+
+// i++ (i=i+1) (Post Increment) Pehlay output generate krayga then add hoga
+// ++i ( Pre Increment) Pehlay add hoga phir output generate krayga
+
+// i-- (Post Decrement)
+// --i (Pre Decrement )
+
+// var a = 3;
+// var b = a++;
+// alert(b);
+
+// var x = 5;
+// var y = ++x;
+// alert(y);
+
+// var a = 5;
+// var b = a++ + a++;
+//         5
+// alert(b);
+
+var x = 3;
+var y = x++ + --x - x + ++x + x-- + --x - x + x++ + ++x;
+//         3 + 3 - 3 + 4 + 4 + 2 - 2 + 2 + 4
+// alert(y);
+
+// var a = 2;
+// var b = --a + ++a - a++ - a++ + a + a++ + a;
+//         1 + 2 - 2 - 3 + 4 + 4 + 5
+// alert(b);
+
+// var f = 10;
+// var g = f%3;
+// alert(g);
