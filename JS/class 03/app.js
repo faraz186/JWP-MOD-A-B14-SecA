cdebdb5c2146fe9d159363bcbe6b397a47606be2
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-12 21:21:10 +0500

    Chapter no 6 - 9 complete...

---
+++ JS/class 03/app.js
@@ -0,0 +1,245 @@
+// variables
+
+// Q calculate circumference of circle in which the given
+
+// radius is 22
+
+var radius = 22;
+
+var circumference = 2 * 3.141 * radius;
+
+console.log("The value is: ", circumference);
+
+console.log(100 + 50);
+
+// variable name = variable value
+
+// variable Declaration
+
+// ES5 (BASIC JS)
+// (BASIC JS)
+// VANILLA JS
+// COMMON JS
+
+// ES6 (ADVANCE JS)
+
+var num;
+
+num = 100;
+
+console.log(num)
+
+// variable Decalration and intialization
+
+var num = 10;
+
+
+// Datatypes in JS
+
+
+// Number   52 , 100 , 50
+
+// STRING   ""
+
+// undefined
+
+// null
+
+
+
+
+var firstNum = "faraz";
+
+var secondNum = null;
+
+console.log(firstNum + secondNum);
+
+console.log(typeof (firstNum + secondNum));
+
+// ------------------------------------------------------
+
+var word = "Hello my name is faraz";
+
+
+
+
+// Camel Case
+
+// var firstName
+
+// var lastName
+
+// var userName
+
+
+
+// Snake Case
+
+// var first_name
+
+// var last_name
+
+// var user_name
+
+
+
+// Pascal Case
+
+// var FirstName
+
+// var LastName
+
+// var UserName
+
+
+
+var firstname = "Muhammad"
+
+var exp = (10 - 5) * 2 + 15;
+
+console.log(exp);
+
+
+
+// BODMAS(Bracket, Order, Divide, Multi, Add, Sub);
+
+
+
+// Increment / Decrement
+
+// i++ => (Post Increment)
+
+// ++i => (Pre Increment)
+
+// i-- => (Post Decrement)
+
+// --i => (Pre Decrement
+
+var num = 10;
+
+var Inc = num--;
+
+console.log(num);
+
+var num2 = num + 1;
+
+var a = 5;
+
+var b = 3;
+
+var c = a + --a + b-- + b + --a + ++a - b;
+
+console.log(c);
+
+var firstName = "Muhammad";
+
+var lastName = "faraz";
+
+var age = 24;
+
+console.log("hello my name is Muhammad faraz and I'm 24 years old.");
+
+console.log(
+  "Hello my name is " +
+    firstName +
+    " " +
+    lastName +
+    "and I'm" +
+    age +
+    " years old."
+);
+
+// prompt
+
+// prompt() =======>   js builtin function
+
+prompt("Enter email", "faraz@gmail.com");
+
+prompt("Enter password");
+
+var firstNum = prompt("Enter first value");
+
+var secondNum = prompt("Enter second value");
+
+console.log(firstNum, secondNum);
+
+var firstName = prompt("Enter first name");
+
+var lastName = prompt("Enter last name");
+
+console.log(firstName, lastName);
+
+var firstNum = +prompt("Enter first value");
+
+var secondNum = +prompt("Enter second value");
+
+console.log("Subtract of two values is:", firstNum - secondNum);
+
+var firstName = "Muhammad";
+
+var lastName = "faraz";
+
+var age = 24;
+
+alert(
+  "Hello my name is " + firstName + lastName + "and I'm" + age + " years old."
+);
+
+// Chapter 1 (Alert)
+
+// q1
+
+alert("Welcome to our website");
+
+// q2
+
+alert("Error! please enter a valid password");
+
+// q3
+
+alert("Welcome to JS Land... \n Happy Coding");
+
+// q4
+
+alert("Welcome to JS Land... ");
+alert("Happy Coding");
+
+// Q6 q7 done
+
+// Chapter 1 end (Alert)
+
+// Chapter 2  (variables for string)
+
+// q1
+
+var username;
+
+// q2
+
+var myName = "Muhammad Faraz";
+
+// q3
+
+var message;
+
+message = "Hello World";
+
+alert(message);
+
+// q5
+
+// alert("           PIZZA \n     PIZZ \n     PIZ \n PI \n P");
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬'
+
+// document.write("<h1>hello <br> world</h1>");
+
+
+var first_name = 10,username = "faraz",fullname = undefined
+
+
+// var username
+
+// var fullname
