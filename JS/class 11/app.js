ae7767f4e346131bbf8f9d4748e2278ac728d24c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-04-03 21:25:15 +0500

    chapter 41 - 47 complete...

---
+++ JS/class 11/app.js
@@ -0,0 +1,64 @@
+// function add(num1, num2) {
+//   var result = num1 + num2;
+
+//   return result;
+// }
+
+// var ResultBack = add(12.54, 54);
+
+// console.log(ResultBack);
+
+// console.log(add(12.54, 54));
+
+// while loop
+
+// var i = 0;
+
+// while ( i <= 10 ) {
+//     console.log(i);
+//     i++
+// }
+
+// do-while loop
+
+// var i = 0;
+
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<=10)
+
+// for loop
+
+// for (var i = 5; i < 5; i++) {
+//   console.log(i);
+// }
+
+// var i = 5;
+
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// var i = 5;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
+
+// Events: Link
+
+function greet() {
+  Swal.fire({
+    title: "Good job!",
+    text: "Form has been submitted!",
+    icon: "success",
+  });
+}
+
+function sum(a, b) {
+  console.log(a + b);
+}
